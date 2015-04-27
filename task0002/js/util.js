cfd4f9e48034255ff7428b91a4cbba6a3f7387f6
DIYgod <diy.d.god@gmail.com>
2015-04-27 15:12:32 +0800

    doing task0002

--- task0002/js/util.js
+++ task0002/js/util.js
@@ -79,10 +79,6 @@ console.log(tarObj.b.b1[0]);    // "hello"
 // task 2.3
 // 对数组进行去重操作，只考虑数组中元素为数字或字符串，返回一个去重后的数组
 function uniqArray(arr) {
-    if (!isArray(arr)) {
-        console.log('Wrong Object!');
-        return null;
-    }
     var new_array = [];
     for (var i = 0, len = arr.length; i < len; i++) {
         if (new_array.indexOf(arr[i]) < 0 ) {
@@ -102,10 +98,6 @@ console.log(b); // [1, 3, 5, 7]
 // 假定空白字符只有半角空格、Tab
 // 练习通过循环，以及字符串的一些基本方法，分别扫描字符串str头部和尾部是否有连续的空白字符，并且删掉他们，最后返回一个完成去除的字符串
 function simpleTrim(str) {
-    if (typeof str !== 'string') {
-        console.log('Wrong Object!');
-        return null;
-    }
     var len = str.length;
     for (var i = 0; i < len && (str.charAt(i) === ' ' || str.charAt(i) === '\t'); i++);
     if (i === len) {
@@ -119,10 +111,6 @@ function simpleTrim(str) {
 // 对字符串头尾进行空格字符的去除、包括全角半角空格、Tab等，返回一个字符串
 // 尝试使用一行简洁的正则表达式完成该题目
 function trim(str) {
-    if (typeof str !== 'string') {
-        console.log('Wrong Object!');
-        return null;
-    }
     return str.replace(/^\s+|\s+$/g, '');
 }
 
@@ -133,10 +121,6 @@ console.log(str); // 'hi!'
 
 // 实现一个遍历数组的方法，针对数组中每一个元素执行fn函数，并将数组索引和元素作为参数传递
 function each(arr, fn) {
-    if (!isArray(arr) || !isFunction(fn)) {
-        console.log('Wrong Object!');
-        return null;
-    }
     for (var i = 0, len = arr.length; i < len; i++) {
         fn(arr[i], i);
     }
@@ -160,10 +144,6 @@ each(arr, output);  // 0:java, 1:c, 2:php, 3:html
 
 // 获取一个对象里面第一层元素的数量，返回一个整数
 function getObjectLength(obj) {
-    if (typeof obj !== 'object') {
-        console.log('Wrong Object!');
-        return null;
-    }
     var element = 0;
     for (var key in obj) {
         if (obj.hasOwnProperty(key)) {
@@ -183,6 +163,18 @@ var obj = {
     }
 };
 console.log(getObjectLength(obj)); // 3
+
+// task 2.4
+// 判断是否为邮箱地址
+function isEmail(emailStr) {
+    return emailStr.search(/^[a-z0-9]([-_\.]?[a-z0-9]+)*@([-_]?[a-z0-9]+)+[\.][a-z]{2,7}([\.][a-z]{2})?$/i) !== -1;
+}
+
+// 判断是否为手机号
+function isMobilePhone(phone) {
+    phone = phone + '';
+    return phone.search(/^(13[0-9]|15[012356789]|17[678]|18[0-9]|14[57])[0-9]{8}$/) !== -1;
+}
 
 
 // task 3.1
@@ -205,11 +197,77 @@ function removeClass(element, oldClassName) {
 
 // 判断siblingNode和element是否为同一个父元素下的同一级的元素，返回bool值
 function isSiblingNode(element, siblingNode) {
-    // your implement
+    return element.parentNode === siblingNode.parentNode;
 }
 
 // 获取element相对于浏览器窗口的位置，返回一个对象{x, y}
 function getPosition(element) {
-    // your implement
+    var left = element.offsetLeft;
+    var top = element.offsetTop;
+    var parent = element.offsetParent;
+
+    while (parent !== null) {
+        left += parent.offsetLeft;
+        top += parent.offsetTop;
+        parent = parent.offsetParent;
+    }
+
+    var scrollLeft = document.body.scrollLeft + document.documentElement.scrollLeft;
+    var scrollTop = document.body.scrollTop + document.documentElement.scrollTop;
+
+    left -= scrollLeft;
+    top -= scrollTop;
+
+    return {
+        x: left,
+        y: top
+    }
 }
-// your implement
+
+
+// task 3.2
+// 实现一个简单的Query
+function $(selector) {
+    var sele = selector.replace(/\s+/, ' ').split(' ');    // 去除多余的空格并分割
+    if (sele[0][0] === '#') {
+        ele = document.getElementById('')
+    }
+    else if (sele[0][0] === '.') {
+
+    }
+    else if (sele[0][0] === '[') {
+
+    }
+    else {
+
+    }
+
+    if (sele.length > 1) {    // 处理组合
+        for (var i = 1, len = sele.length; i < len; i++) {
+            ele = getEle(ele, sele[i]);
+        }
+    }
+
+    var getEle = function (ele, onesele) {
+
+    }
+
+    return ele;
+}
+
+// 可以通过id获取DOM对象，通过#标示，例如
+$("#adom"); // 返回id为adom的DOM对象
+
+// 可以通过tagName获取DOM对象，例如
+$("a"); // 返回第一个<a>对象
+
+// 可以通过样式名称获取DOM对象，例如
+$(".classa"); // 返回第一个样式定义包含classa的对象
+
+// 可以通过attribute匹配获取DOM对象，例如
+$("[data-log]"); // 返回第一个包含属性data-log的对象
+
+$("[data-time=2015]"); // 返回第一个包含属性data-time且值为2015的对象
+
+// 可以通过简单的组合提高查询便利性，例如
+$("#adom .classa"); // 返回id为adom的DOM所包含的所有子节点中，第一个样式定义包含classa的对象
