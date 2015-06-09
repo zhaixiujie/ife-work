88115ec5ce58a22f180c5da6cc1c2b703eb2d6b8
DIYgod <diy.d.god@gmail.com>
2015-06-09 22:05:03 +0800

    task0004 done

--- task0004/Gruntfile.js
+++ task0004/Gruntfile.js
@@ -189,7 +189,7 @@ module.exports = function (grunt) {
       dist: {
         files: {
           src: [
-            '<%= config.dist %>/scripts/{,*/}*.js',
+            // '<%= config.dist %>/scripts/{,*/}*.js',
             '<%= config.dist %>/styles/{,*/}*.css',
             '<%= config.dist %>/images/{,*/}*.*',
             // '<%= config.dist %>/font/{,*/}*.*',
---
+++ task0004/dist/scripts/main.js
@@ -0,0 +1,1 @@
+require(["gtd"],function(a){a.init(),a.makeType()});
