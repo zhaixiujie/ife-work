20ec30226c59929ac1419c8b43ededc909bcfe46
范文杰 <diy.d.god@gmail.com>
2015-09-21 00:44:59 +0800

    review util.js

--- task0002/js/util.js
+++ task0002/js/util.js
@@ -4,19 +4,13 @@
 // task 2.1
 // 判断arr是否为一个数组，返回一个bool值
 function isArray(arr) {
-    // return arr instanceof Array;  //简易判断，准确度差
+    // return Array.isArray(arr);
     return Object.prototype.toString.call(arr) === '[object Array]';
 }
 
 // 判断fn是否为一个函数，返回一个bool值
 function isFunction(fn) {
-    // typeof fn === 'function'  //简易判断，准确度差
-    return !!fn
-        && !fn.nodeName
-        && fn.constructor != String
-        && fn.constructor != RegExp
-        && fn.constructor != Array
-        && /function/i.test(fn + '');
+    return Object.prototype.toString.call(fn) === '[object Function]';
 }
 
 
@@ -24,29 +18,28 @@ function isFunction(fn) {
 // 使用递归来实现一个深度克隆，可以复制一个目标对象，返回一个完整拷贝
 // 被复制的对象类型会被限制为数字、字符串、布尔、日期、数组、Object对象。不会包含函数、正则对象等
 function cloneObject(src) {
-    // 对于 数字 字符串 布尔 null undefined
-    if (src == null || typeof src != 'object') {
-        return src;
-    }
+    var clone = src;
 
+    // 对于Date,String,Boolean等引用类型的数据，需要考虑调用构造函数重新构造，直接赋值依然会有引用问题（不是真正的clone引用变量）
     // 对于 Date
     if (src instanceof Date) {
-        var clone = new Date(src.getDate());
+        clone = new Date(src.getDate());
         return clone;
     }
 
+    // 对于Object和Array的遍历，可以使用for in，这样可以保证在在Array对象上扩展的属性也可以正确复制
     // 对于 数组
     if (src instanceof Array) {
-        var clone = [];
-        for (var i = 0, len = src.length; i < len; i++) {
-            clone[i] = src[i];
+        clone = [];
+        for (var key in src) {
+            clone[key] = cloneObject(src[key]);
         }
         return clone;
     }
 
     // 对于 Object
     if (src instanceof Object) {
-        var clone = {};
+        clone = {};
         for (var key in src) {
             if (src.hasOwnProperty(key)) {       // 忽略掉继承属性
                 clone[key] = cloneObject(src[key]);
@@ -54,6 +47,9 @@ function cloneObject(src) {
         }
         return clone;
     }
+
+    // 对于 数字 字符串 布尔 null undefined
+    return src;
 }
 
 // 测试用例：
@@ -81,10 +77,59 @@ console.log(tarObj.b.b1[0]);    // "hello"
 
 // task 2.3
 // 对数组进行去重操作，只考虑数组中元素为数字或字符串，返回一个去重后的数组
-function uniqArray(arr) {
+function uniqArray(source) {
+    var len = source.length,
+        result = source.slice(0),
+        i, datum;
+
+
+    // 从后往前双重循环比较
+    // 如果两个元素相同，删除后一个
+    while (--len > 0) {
+        datum = result[len];
+        i = len;
+        while (i--) {
+            if (datum === result[i]) {
+                result.splice(len, 1);
+                break;
+            }
+        }
+    }
+
+    return result;
+}
+
+// hash
+function uniqArray1(arr) {
+    var obj = {};
+    var result = [];
+    for (var i = 0, len = arr.length; i < len; i++) {
+
+        var key = arr[i];
+
+        if (!obj[key]) {
+            result.push(key);
+            obj[key] = true;
+        }
+    }
+    return result;
+}
+
+
+// hash + es5
+// 速度最快
+function uniqArray2(arr) {
+    var obj = {};
+    for (var i = 0, len = arr.length; i < len; i++) {
+        obj[arr[i]] = true;
+    }
+    return Object.keys(obj);
+}
+
+function uniqArray3(arr) {
     var new_array = [];
     for (var i = 0, len = arr.length; i < len; i++) {
-        if (arr[i] !== '' && new_array.indexOf(arr[i]) < 0 ) {
+        if (arr[i] !== '' && new_array.indexOf(arr[i]) < 0 ) {    // indexOf方法不支持IE9以下
             new_array.push(arr[i]);
         }
     }
@@ -95,7 +140,28 @@ function uniqArray(arr) {
 /*
 var a = [1, 3, 5, 7, 5, 3];
 var b = uniqArray(a);
-console.log(b); // [1, 3, 5, 7]
+
+var al = 10000;
+var a = [];
+while (al--){
+a.push(al%2);
+}
+
+console.time('uniqArray')
+console.log(uniqArray(a).length);
+console.timeEnd('uniqArray')
+
+console.time('uniqArray1')
+console.log(uniqArray1(a).length);
+console.timeEnd('uniqArray1')
+
+console.time('uniqArray2')
+console.log(uniqArray2(a).length);
+console.timeEnd('uniqArray2')
+
+console.time('uniqArray3')
+console.log(uniqArray3(a).length);
+console.timeEnd('uniqArray3')
 */
 
 // 中级班同学跳过此题
@@ -103,12 +169,16 @@ console.log(b); // [1, 3, 5, 7]
 // 假定空白字符只有半角空格、Tab
 // 练习通过循环，以及字符串的一些基本方法，分别扫描字符串str头部和尾部是否有连续的空白字符，并且删掉他们，最后返回一个完成去除的字符串
 function simpleTrim(str) {
+    function isEmpty(c) {
+        return /\s/.test(c);
+    }
+
     var len = str.length;
-    for (var i = 0; i < len && (str.charAt(i) === ' ' || str.charAt(i) === '\t'); i++);
+    for (var i = 0; i < len && isEmpty(str.charAt(i)); i++);
     if (i === len) {
         return '';
     }
-    for (var j = len; j && (str.charAt(j - 1) === ' ' || str.charAt(j - 1) === '\t'); j--);
+    for (var j = len; j && isEmpty(str.charAt(j - 1)); j--);
     return str.substring(i, j);
 }
 
@@ -152,6 +222,13 @@ each(arr, output);  // 0:java, 1:c, 2:php, 3:html
 */
 
 // 获取一个对象里面第一层元素的数量，返回一个整数
+/**
+ * getObjectLength 在for in的时候，要了解在IE9以下，有枚举bug。
+ * a = {toString:1}时，for in不出toString这个key。
+ * 查找关于propertyIsEnumerable的使用方法，来判断
+ * 'toString' 'toLocaleString' 'valueOf' 'hasOwnProperty' 'isPrototypeOf' 'propertyIsEnumerable' ‘constructor'
+ * 这几个不可枚举(for in)出来的key
+ */
 function getObjectLength(obj) {
     var element = 0;
     for (var key in obj) {
@@ -178,19 +255,21 @@ console.log(getObjectLength(obj)); // 3
 // task 2.4
 // 判断是否为邮箱地址
 function isEmail(emailStr) {
+    // return (/^[a-z0-9]([-_\.]?[a-z0-9]+)*@([-_]?[a-z0-9]+)+[\.][a-z]{2,7}([\.][a-z]{2})?$/i).test(emsilStr);
     return emailStr.search(/^[a-z0-9]([-_\.]?[a-z0-9]+)*@([-_]?[a-z0-9]+)+[\.][a-z]{2,7}([\.][a-z]{2})?$/i) !== -1;
 }
 
 // 判断是否为手机号
 function isMobilePhone(phone) {
     phone = phone + '';
+    // return (/^(13[0-9]|15[012356789]|17[678]|18[0-9]|14[57])[0-9]{8}$/).test(phone);
     return phone.search(/^(13[0-9]|15[012356789]|17[678]|18[0-9]|14[57])[0-9]{8}$/) !== -1;
 }
 
 
 // task 3.1
 function hasClass(element, className) {
-    var name = element.className.split(' ');
+    var name = element.className.match(/\S+/g) || [];
     if (name.indexOf(className) !== -1) {
         return true;
     }
@@ -207,7 +286,7 @@ function addClass(element, newClassName) {
 // 移除element中的样式oldClassName
 function removeClass(element, oldClassName) {
     if (hasClass(element, oldClassName)) {
-        element.className = element.className.replace(oldClassName, '');
+        element.className = trim(element.className.replace(oldClassName, ''));
     }
 }
 
@@ -218,25 +297,27 @@ function isSiblingNode(element, siblingNode) {
 
 // 获取element相对于浏览器窗口的位置，返回一个对象{x, y}
 function getPosition(element) {
-    var left = element.offsetLeft;
-    var top = element.offsetTop;
-    var parent = element.offsetParent;
+    var x = 0;
+    var y = 0;
+    var current = element;
 
-    while (parent !== null) {
-        left += parent.offsetLeft;
-        top += parent.offsetTop;
-        parent = parent.offsetParent;
+    while (current !== null) {
+        x += current.offsetLeft;
+        y += current.offsetTop;
+        current = current.offsetParent;
     }
 
     var scrollLeft = document.body.scrollLeft + document.documentElement.scrollLeft;
     var scrollTop = document.body.scrollTop + document.documentElement.scrollTop;
 
-    left -= scrollLeft;
-    top -= scrollTop;
+    // element.getBoundingClientRect()
+
+    x -= scrollLeft;
+    y -= scrollTop;
 
     return {
-        x: left,
-        y: top
+        x: x,
+        y: y
     }
 }
 
@@ -245,47 +326,33 @@ function getPosition(element) {
 // 实现一个简单的Query
 function $(selector) {
     var allchilds = [];
-    var childs = function (element) {    // 递归获取所有子元素
+    var childs = function (element) {    // 获取所有子元素
         return element.getElementsByTagName('*');
-    }
+    };
 
-    var ele = document.getElementsByTagName('html')[0];    // 获取所有元素
+    var ele = document;
     var sele = selector.replace(/\s+/, ' ').split(' ');    // 去除多余的空格并分割
 
     for (var i = 0, len = sele.length; i < len; i++) {
-        ele = childs(ele);
         var eleLen = ele.length;
         var isGet = false;
 
         switch (sele[i][0]) {    // 从子节点中查找
             case '#':
-                for (var j = 0; j < eleLen; j++) {
-                    if (ele[j].id === sele[i].substring(1)) {
-                        ele = ele[j];
-                        isGet = true;
-                        break;
-                    }
-                }
+                ele = ele.getElementById(sele[i].substring(1));
                 break;
             case '.':
-                for (var j = 0; j < eleLen; j++) {
-                    var name = uniqArray(ele[j].className.split(' '));
-                    if (name.indexOf(sele[i].substring(1)) !== -1) {
-                        ele = ele[j];
-                        isGet = true;
-                        break;
-                    }
-                }
+                ele = ele.getElementsByClassName(sele[i].substring(1))[0];
                 break;
             case '[':
                 var valueLoc = sele[i].indexOf('=');
+                var temp = ele.getElementsByTagName('*');
                 if (valueLoc !== -1) {
                     var key = sele[i].substring(1, valueLoc);
                     var value = sele[i].substring(valueLoc + 1, sele[i].length - 1);
                     for (var j = 0; j < eleLen; j++) {
-                        if (ele[j][key] === value) {
-                            ele = ele[j];
-                            isGet = true;
+                        if (temp[j][key] === value) {
+                            ele = temp[j];
                             break;
                         }
                     }
@@ -293,27 +360,20 @@ function $(selector) {
                 else {
                     var key = sele[i].substring(1, sele[i].length - 1);
                     for (var j = 0; j < eleLen; j++) {
-                        if (ele[j][key]) {
-                            ele = ele[j];
-                            isGet = true;
+                        if (temp[j][key]) {
+                            ele = temp[j];
                             break;
                         }
                     }
                 }
                 break;
             default :
-                for (var j = 0; j < eleLen; j++) {
-                    if (ele[j].tagName === sele[i].toUpperCase()) {    // tagName 属性的返回值始终是大写的
-                        ele = ele[j];
-                        isGet = true;
-                        break;
-                    }
-                }
+                ele = ele.getElementsByTagName(sele[i])[0];
                 break;
         }
     }
 
-    if (!isGet) {
+    if (!ele) {
         ele = null;
     }
 
@@ -342,27 +402,27 @@ $("#adom .classa"); // 返回id为adom的DOM所包含的所有子节点中，第
 // task 4.1
 // 给一个element绑定一个针对event事件的响应，响应函数为listener
 function addEvent(element, event, listener) {
-    element['on' + event] = listener;
+    element.addEventListener('on' + event, listener);
 }
 
 // 移除element对象对于event事件发生时执行listener的响应
 function removeEvent(element, event, listener) {
-    element['on' + event] = null;
+    element.removeEventListener('on' + event, listener);
 }
 
 // 实现对click事件的绑定
 function addClickEvent(element, listener) {
-    element.onclick = listener;
+    addEvent(element, 'click', listener);
 }
 
 // 实现对于按Enter键时的事件绑定
 function addEnterEvent(element, listener) {
-    element.onkeydown = function(e) {
+    addEvent(element, 'keydown', function(e) {
         e = e || window.event;
         if (e.keyCode === 13) {
             listener();
         }
-    }
+    });
 }
 
 // 接下来我们把上面几个函数和$做一下结合，把他们变成$对象的一些方法
@@ -406,14 +466,14 @@ function init() {
 // 我们增加了一个按钮，当点击按钮时，改变list里面的项目，这个时候你再点击一下li，绑定事件不再生效了。
 // 那是不是我们每次改变了DOM结构或者内容后，都需要重新绑定事件呢？当然不会这么笨，接下来学习一下事件代理，然后实现下面新的方法。
 function delegateEvent(element, tag, eventName, listener) {
-    element['on' + eventName] = function(e) {
-        var e = e || window.event;
-        var target = e.srcElement ? e.srcElement : e.target;
-        var tname = target.nodeName.toLowerCase();
-        if (tname === tag) {
-            target['on' + eventName] = listener;
+    addEvent(element, eventName, function (e) {
+        var event = e || window.event;
+        var target = event.target || event.srcElement;
+
+        if (target && target.tagName === tag.toUpperCase()) {
+            listener.call(target, event);
         }
-    }
+    });
 }
 
 $.delegate = delegateEvent;
@@ -427,6 +487,7 @@ $.delegate($("#list"), "li", "click", clickListener);
 // task 5.1
 // 判断是否为IE浏览器，返回-1或者版本号
 function isIE() {
+    /*
     var ua = navigator.userAgent.toLowerCase();
     var ie = ua.match(/rv:([\d.]+)/) || ua.match(/msie ([\d.]+)/);
     if(ie) {
@@ -435,25 +496,66 @@ function isIE() {
     else {
         return -1;
     }
+    */
+    return /msie (\d+\.\d+)/i.test(navigator.userAgent)
+        ? (document.documentMode || + RegExp['\x241']) : undefined;
 }
 
+
 // 设置cookie
+function isValidCookieName(cookieName) {
+    // http://www.w3.org/Protocols/rfc2109/rfc2109
+    // Syntax:  General
+    // The two state management headers, Set-Cookie and Cookie, have common
+    // syntactic properties involving attribute-value pairs.  The following
+    // grammar uses the notation, and tokens DIGIT (decimal digits) and
+    // token (informally, a sequence of non-special, non-white space
+    // characters) from the HTTP/1.1 specification [RFC 2068] to describe
+    // their syntax.
+    // av-pairs   = av-pair *(";" av-pair)
+    // av-pair    = attr ["=" value] ; optional value
+    // attr       = token
+    // value      = word
+    // word       = token | quoted-string
+
+    // http://www.ietf.org/rfc/rfc2068.txt
+    // token      = 1*<any CHAR except CTLs or tspecials>
+    // CHAR       = <any US-ASCII character (octets 0 - 127)>
+    // CTL        = <any US-ASCII control character
+    //              (octets 0 - 31) and DEL (127)>
+    // tspecials  = "(" | ")" | "<" | ">" | "@"
+    //              | "," | ";" | ":" | "\" | <">
+    //              | "/" | "[" | "]" | "?" | "="
+    //              | "{" | "}" | SP | HT
+    // SP         = <US-ASCII SP, space (32)>
+    // HT         = <US-ASCII HT, horizontal-tab (9)>
+
+    return (new RegExp('^[^\\x00-\\x20\\x7f\\(\\)<>@,;:\\\\\\\"\\[\\]\\?=\\{\\}\\/\\u0080-\\uffff]+\x24'))
+        .test(cookieName);
+}
+
 function setCookie(cookieName, cookieValue, expiredays) {
+    if (!isValidCookieName(cookieName)) {
+        return;
+    }
+
+    var exdate = '';
     if (expiredays) {
-        var exdate = new Date();
+        exdate = new Date();
         exdate.setDate(exdate.getDate() + expiredays);
-        var expires = ';expires=' + exdate.toUTCString();
+        var expires = ';expires=' + exdate.toUTCString();     // toGMTString is deprecated and should no longer be used, it's only there for backwards compatibility, use toUTCString() instead
     }
-    else {
-        expires = '';
-    }
-    document.cookie = cookieName + '=' + escape(cookieValue) + expires;
+    document.cookie = cookieName + '=' + encodeURIComponent(cookieValue) + expires;    // 废弃的 escape() 方法生成新的由十六进制转移序列替换的字符串. 使用 encodeURI 或 encodeURIComponent 代替
 }
 
 // 获取cookie值
 function getCookie(cookieName) {
+    if (!isValidCookieName(cookieName)) {
+        return null;
+    }
+
     var re = new RegExp(cookieName + '=(.*?)($|;)');
-    return re.exec(document.cookie)[1];
+    return re.exec(document.cookie)[1] || null;
 }
 
 // task 6.1
