c78a9c33fb1414dc8937525efdc853e7c28d70dd
范文杰 <diy.d.god@gmail.com>
2015-09-21 16:47:22 +0800

    debug $

--- task0002/js/util.js
+++ task0002/js/util.js
@@ -325,17 +325,10 @@ function getPosition(element) {
 // task 3.2
 // 实现一个简单的Query
 function $(selector) {
-    var allchilds = [];
-    var childs = function (element) {    // 获取所有子元素
-        return element.getElementsByTagName('*');
-    };
-
     var ele = document;
     var sele = selector.replace(/\s+/, ' ').split(' ');    // 去除多余的空格并分割
 
     for (var i = 0, len = sele.length; i < len; i++) {
-        var eleLen = ele.length;
-        var isGet = false;
 
         switch (sele[i][0]) {    // 从子节点中查找
             case '#':
@@ -347,10 +340,11 @@ function $(selector) {
             case '[':
                 var valueLoc = sele[i].indexOf('=');
                 var temp = ele.getElementsByTagName('*');
+                var tLen = temp.length;
                 if (valueLoc !== -1) {
                     var key = sele[i].substring(1, valueLoc);
                     var value = sele[i].substring(valueLoc + 1, sele[i].length - 1);
-                    for (var j = 0; j < eleLen; j++) {
+                    for (var j = 0; j < tLen; j++) {
                         if (temp[j][key] === value) {
                             ele = temp[j];
                             break;
@@ -359,7 +353,7 @@ function $(selector) {
                 }
                 else {
                     var key = sele[i].substring(1, sele[i].length - 1);
-                    for (var j = 0; j < eleLen; j++) {
+                    for (var j = 0; j < tLen; j++) {
                         if (temp[j][key]) {
                             ele = temp[j];
                             break;
